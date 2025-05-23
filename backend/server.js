d54571724744d6eedd66747207f4c3207361ea53
kmrathod29 <kmrathod5787@gmail.com>
2025-05-23 10:13:48 +0530

    Update real link of frontend

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5500', 
+  origin: 'https://custom-core-backend.onrender.com/#home', 
   credentials: true
 }));
 
